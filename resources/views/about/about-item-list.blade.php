73d464fbef1cf3c2e65b13ea627cd47338e2756d
ersurajkumar1999 <er.surajkumar1999@gmail.com>
2024-07-17 00:12:34 +0530

    about list

--- resources/views/about/about-item-list.blade.php
+++ resources/views/about/about-item-list.blade.php
@@ -1,30 +1,52 @@
 <div class="row">
     <div class="col-md-12">
         <div class="card">
-            <h5 class="card-header">Roles List</h5>
-            <div>
-                <a href="">add</a>
+            <h5 class="card-header">About List</h5>
+            <div class="add-new-btn">
+
+                <button class="btn btn-primary">Add Item</button>
             </div>
             <div class="table-responsive text-nowrap">
-                <table class="table">
-                <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Display Name</th>
-                    <th>Description</th>
-                    <th>Actions</th>
-                </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                        <td>name </td>
-                        <td>display_name</td>
-                        <td>description</td>
-                        <td>description</td>
-                    </tr>
-                </tbody>
+                <table class="table" id="userTable">
+                    <thead>
+                        <tr>
+                            <th>#</th>
+                            <th>Text</th>
+                            <th>Icon</th>
+                            <th>Number</th>
+                            <th>Status</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($about->items as $key=> $item)
+                        <tr>
+                            <td>{{ $key + 1 }}</td>
+                            <td>{{ $item->text }}</td>
+                            <td>{{ $item->icon }}</td>
+                            <td>{{ $item->number }}</td>
+                            <td>{{ $item->status }}</td>
+                            <td>
+                                <button class="btn btn-primary">Edit</button>
+                                <button class="btn btn-danger">Delete</button>
+                            </td>
+                        </tr>
+                        @endforeach
+                        @if ($about->items->isEmpty())
+                        <tr>
+                            <td colspan="5">No items found.</td>
+                        </tr>
+                        @endif
+                    </tbody>
                 </table>
             </div>
         </div>
     </div>
 </div>
+
+<script type="text/javascript">
+    $(document).ready( function () {
+        // $('#userTable').DataTable();
+        var table = $('#userTable').DataTable();
+    } );
+</script>
